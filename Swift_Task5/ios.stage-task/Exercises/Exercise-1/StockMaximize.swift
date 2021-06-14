e7fc53660145f05b8819c0883337e013a4e10fed
AnChugunov <justtobehappy123@gmail.com>
2021-06-14 22:46:47 +0300

    excs 1, 2

--- Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
@@ -3,6 +3,23 @@ import Foundation
 class StockMaximize {
 
     func countProfit(prices: [Int]) -> Int {
-        return -1
+        
+        var profit = 0
+        var maxPrice = 0
+        if prices.isEmpty {
+            return profit
+        }
+        for i in 0..<prices.count {
+            maxPrice = 0
+            for j in i + 1..<prices.count {
+                
+                if (prices[j] - prices[i] > maxPrice) {
+                    
+                    maxPrice = prices[j] - prices[i]
+                }
+            }
+            profit += maxPrice
+        }
+        return profit
     }
 }
--- Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
@@ -3,6 +3,42 @@ import UIKit
 class MessageDecryptor: NSObject {
     
     func decryptMessage(_ message: String) -> String {
-        return ""
+        
+            var numberString = ""
+            var numbersStack: [Int] = [1]
+            var substringsStack: [String] = [""]
+            var lastInNumbersStack: Int
+            var lastInSubstringsStack: String
+            for i in message {
+               
+                if i.isNumber {
+                    numberString.append(i)
+                    continue
+                }
+              
+                if i == "[" {
+                    numbersStack.append(Int(numberString) ?? 1)
+                    substringsStack.append("")
+                    numberString = ""
+                    continue
+                }
+                
+                if i == "]" {
+                    lastInNumbersStack = numbersStack.popLast()!
+                    lastInSubstringsStack = substringsStack.popLast()!
+                    var stringToAppend: String = ""
+                    for _ in 0...lastInNumbersStack {
+                        stringToAppend += lastInSubstringsStack
+                    }
+                    substringsStack.append(stringToAppend)
+                    continue
+                }
+                substringsStack[substringsStack.count - 1].append(i)
+            }
+            
+            
+            return substringsStack.first!
+        
     }
-}
+        
+    }
