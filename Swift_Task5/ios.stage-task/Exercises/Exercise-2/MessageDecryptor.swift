e72af7f3ad4086e0f347a315ede8863991037d97
AnChugunov <justtobehappy123@gmail.com>
2021-06-15 10:44:44 +0300

    fixed again

--- Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
@@ -1,37 +1,44 @@
 import UIKit
 
 class MessageDecryptor: NSObject {
-    
-    func decryptMessage (_ message: String) -> String {
-        if message.count > 60 {
-            return ""
-        }
-        struct Stack {
-            var number: Int
-            var subMessage: String
-        }
-        var stackArray : [Stack] = []
-        stackArray.append(Stack(number: 1, subMessage: ""))
-        var numberString: String = ""
-        for i in message {
-            if i.isNumber {
-                numberString.append(i)
-                continue
-            }
-            if i == "[" {
-                stackArray.append(Stack(number: Int(numberString) ?? 1, subMessage: ""))
-                numberString = ""
-                continue
-            }
-            if i == "]" {
-                if let lastInStack = stackArray.popLast() {
-                    stackArray[stackArray.count - 1].subMessage.append(String(repeating: lastInStack.subMessage, count: lastInStack.number))
-                    continue
-                }
-            }
-            stackArray[stackArray.count - 1].subMessage.append(i)
-        }
-        return stackArray.first?.subMessage ?? ""
-    }
-   
-}
+
+     func decryptMessage(_ message: String) -> String {
+
+             var numberString = ""
+             var numbersStack: [Int] = [1]
+             var substringsStack: [String] = [""]
+             var lastInNumbersStack: Int
+             var lastInSubstringsStack: String
+             for i in message {
+
+                 if i.isNumber {
+                     numberString.append(i)
+                     continue
+                 }
+
+                 if i == "[" {
+                     numbersStack.append(Int(numberString) ?? 1)
+                     substringsStack.append("")
+                     numberString = ""
+                     continue
+                 }
+
+                 if i == "]" {
+                     lastInNumbersStack = numbersStack.popLast()!
+                     lastInSubstringsStack = substringsStack.popLast()!
+                     var stringToAppend: String = ""
+                     for _ in 0...lastInNumbersStack {
+                         stringToAppend += lastInSubstringsStack
+                     }
+                     substringsStack.append(stringToAppend)
+                     continue
+                 }
+                 substringsStack[substringsStack.count - 1].append(i)
+             }
+
+
+             return substringsStack.first!
+
+     }
+
+     }
